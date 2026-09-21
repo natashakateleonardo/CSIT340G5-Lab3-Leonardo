50c11ac1339fdda19a161d821ae87a8ea2b5fc89
tashaleonardo <natashakate.leonardo@cit.edu>
2026-09-21 17:05:35 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-const Part = (props) => <p>{props.name} {props.exercises}</p>
+const Part = (props) => <p>{props.part.name} {props.part.exercises}</p>
 
 const Content = (props) => (
   <div>
-    <Part name={props.part1} exercises={props.exercises1} />
-    <Part name={props.part2} exercises={props.exercises2} />
-    <Part name={props.part3} exercises={props.exercises3} />
+    <Part part={props.part1} />
+    <Part part={props.part2} />
+    <Part part={props.part3} />
   </div>
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
 )
 
 const Footer = (props) => (
@@ -22,12 +22,10 @@ const Footer = (props) => (
 
 const App = () => {
   const course = 'BSIT 4th Year - 1st Semester'
-  const part1 = 'CSIT340 - Industry Elective 1'
-  const exercises1 = 3
-  const part2 = 'FL033 - Nihonggo 1'
-  const exercises2 = 3
-  const part3 = 'ENGL014 - Technical Writing'
-  const exercises3 = 3
+  
+  const part1 = { name: 'CSIT340 - Industry Elective 1', exercises: 3 }
+  const part2 = { name: 'FL033 - Nihonggo 1', exercises: 3 }
+  const part3 = { name: 'ENGL014 - Technical Writing', exercises: 3 }
 
   const studentName = 'Natasha Kate A. Leonardo'
   const courseCode = 'CSIT340'
@@ -36,12 +34,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
       <Footer name={studentName} courseCode={courseCode} section={section} />
     </div>
   )
